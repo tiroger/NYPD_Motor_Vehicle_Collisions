e35f1a1d19a37375cf5268cf40f83275ab699a90
Roger Lefort <tirogerl@gmail.com>
2019-06-21 12:52:06 -0400

    Testing various cleanup strategies

---
+++ README.md
@@ -0,0 +1,2 @@
+# Exploration of Motor Vehicle Collisions Data in NYC
+
---
+++ bike_lane_length_calculator.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+# Define a basic Haversine distance formula
+def haversine(lat1, lon1, lat2, lon2):
+    MILES = 3959
+    lat1, lon1, lat2, lon2 = map(np.deg2rad, [lat1, lon1, lat2, lon2])
+    dlat = lat2 - lat1 
+    dlon = lon2 - lon1 
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a)) 
+    total_miles = MILES * c
+    return total_miles
